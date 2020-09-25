df7122831757ac81813dd66de534330b3a2b7ac8
kawsar00 <kawsarmahmud358@gmail.com>
2020-09-26 00:14:40 +0600

    home page completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import foodItemDetails from './Components/foodItemDetails/foodItemDetails';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+       <Header></Header>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/foodItem/:key">
+          <foodItemDetails></foodItemDetails>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
---
+++ src/Components/foodItemDetails/foodItemDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const foodItemDetails = () => {
+  
+  
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default foodItemDetails;
